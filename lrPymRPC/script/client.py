7eb240b767dc3d4ae34d611784f62eeb8ba36e4d
MATSUDA Masahiro <matsuda@logic-research.co.jp>
2026-01-15 18:16:15 +0900

    DEBUG

--- lrPymRPC/script/client.py
+++ lrPymRPC/script/client.py
@@ -85,6 +85,10 @@ def run(tool="git://github.com", target="help", server_ip="localhost", server_po
     #channel = grpc.insecure_channel('localhost:50052')
     #channel = grpc.insecure_channel('localhost:8765')
 
+    print("client_key:", type(client_key), client_key is None)
+    print("client_crt:", type(client_crt), client_crt is None)
+    print("ca_crt    :", type(ca_crt), ca_crt is None)
+
     # ======== 認証処理 ========
     creds = grpc.ssl_channel_credentials(
         root_certificates=ca_crt,
